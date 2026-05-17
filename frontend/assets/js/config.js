752bca54f7ccc76b57a4bae5694d07ce0949ae67
Pavira <pavivalavan@gmail.com>
2026-05-18 02:07:54 +0530

    updated config.js and main.js

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -2,7 +2,7 @@
  * Global configuration for Agentic Bot frontend
  */
 
-const API_BASE_URL = "https://rag-chatbot-backend-ten.vercel.app/"; // Base URL for API requests
+const API_BASE_URL = "https://rag-chatbot-backend-ten.vercel.app"; // Base URL for API requests
 
 const CONFIG = {
   API_BASE_URL,
